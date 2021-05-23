dcddef3b3854d23be9d5b7369172c8aa0d040be6
Ennin Francis <enninfrancis47@gmail.com>
2021-05-23 21:50:57 +0000

    make it better

--- nsromahub_project_api/settings.py
+++ nsromahub_project_api/settings.py
@@ -173,8 +173,7 @@ REST_FRAMEWORK = {
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    
     # 'DEFAULT_PERMISSION_CLASSES': [
     # 'rest_framework.permissions.IsAuthenticated',
     # ],
