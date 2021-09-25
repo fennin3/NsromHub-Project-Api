41723ce4d233229b5a0fd7f9e171336f987010ec
Francis Ennin <enninfrancis47@gmail.com>
2021-09-25 18:41:20 +0000

    make it better

--- nsromahub_project_api/settings.py
+++ nsromahub_project_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6^4ca9$ml4l1c#36gkc*z3#8(h_(9efte_*(03byw4real%7rl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["198.199.73.102","localhost", "127.0.0.1", 'api.rennintech.com', "nsrohub.herokuapp.com"]
+ALLOWED_HOSTS = ["198.199.73.102","localhost", "127.0.0.1", 'api.rennintech.com', "nsrohub.herokuapp.com", "143.244.153.209"]
 
 
 # Application definition
