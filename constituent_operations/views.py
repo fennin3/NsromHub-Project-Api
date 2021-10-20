63a8eacfd7cac89a880be558669e9ce8ac51571f
Ennin Francis <enninfrancis47@gmail.com>
2021-10-20 09:12:13 +0000

    Update views.py

--- constituent_operations/views.py
+++ constituent_operations/views.py
@@ -672,6 +672,7 @@ class GetActionPlanApprovedStatusView(APIView):
 
                 return Response(data,status=status.HTTP_200_OK)
             except Exception as e:
+                print(e)
                 data = {
                     "status":status.HTTP_400_BAD_REQUEST,
                     "message":f"Action plan summary of {user.active_area.name} was not approved, try again."
